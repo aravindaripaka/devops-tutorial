44b878b370631b08aaf7184adcb80bf72b57145b
aravind-aripaka <abhi.aripaka2002.com>
2026-05-05 16:40:58 -0500

    fix lint and add pytest to requirements

--- server.py
+++ server.py
@@ -4,19 +4,22 @@ import time
 app = Flask(__name__)
 START_TIME = time.time()
 
+
 @app.route("/")
 def home():
     return jsonify({"status": "ok", "message": "Hello from myapp"})
+
 
 @app.route("/health")
 def health():
     uptime = round(time.time() - START_TIME, 2)
     return jsonify({"status": "healthy", "uptime_seconds": uptime})
 
+
 @app.route("/error")
 def error():
     raise Exception("simulated error")
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
-
